b33cfc3d8252d9745572f0850e3e9a96e725a549
arosasvenegas <a.rosasvenegas@gmail.com>
2022-10-08 01:19:38 -0400

    feat: create quick order

---
+++ react/components/QuickOrder.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react'
+import { useMutation, useLazyQuery } from 'react-apollo'
+import UPDATE_CART from '../graphql/updateCart.graphql'
+import GET_PRODUCT from '../graphql/getProductBySku.graphql'
+
+const QuickOrder = () => {
+  const [inputText, setInputText] = useState("");
+  const [search, setSearch] = useState("")
+
+  const [getProductData, { data: product }] = useLazyQuery(GET_PRODUCT)
+  const [addToCart] = useMutation(UPDATE_CART)
+
+  const handleChange = (event: any) => {
+    setInputText(event.target.value)
+    console.log("Input changed", inputText);
+  }
+
+  useEffect(() => {
+    console.log("el resultado producto es", product, search);
+    if (product) {
+      let skuId = parseInt(inputText)
+      console.log("mis datos necesarios", skuId, product)
+      addToCart({
+        variables: {
+          salesChannel: "1",
+          items: [
+            {
+              id: skuId,
+              quantity: 1,
+              seller: "1"
+            }
+          ]
+        }
+      })
+        .then(() => {
+          window.location.href = "/checkout"
+        })
+    }
+  }, [product, search])
+
+  const addProductToCart = () => {
+    getProductData({
+      variables: {
+        sku: inputText
+      }
+    })
+  }
+
+  const searchProduct = (event: any) => {
+    event.preventDefault();
+    if (!inputText) {
+      alert("se ingreso algo")
+    } else {
+      setSearch(inputText)
+      addProductToCart()
+    }
+  }
+  return <div>
+    <h2>Compra rapida de casa ideas</h2>
+    <form onSubmit={(searchProduct)}>
+      <div>
+        <label htmlFor='sku'> Ingresa el numero de SKU</label>
+        <input id='sku' type='text' onChange={handleChange}></input>
+      </div>
+      <input type='submit' value='AÑADIR AL CARRITO' />
+    </form>
+  </div>
+}
+
+export default QuickOrder
